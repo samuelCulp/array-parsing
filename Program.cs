c90726691b92fd9d5c3eecfac1ef30a755395623
samuelCulp <99611615+samuelCulp@users.noreply.github.com>
2022-09-14 21:14:16 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,26 +5,6 @@
 using System;
 
 
-
-//# Objective
-//A client has requested that a Console application be built to receive an unknown quantity of delimited numbers.
-
-//# Limitations
-//* Valid inputs will be Numeric (Whole or Decimal)
-//*Invalid inputs should be discarded and/or ignored
-//* Must be able to enter multiple times
-//    > Need to have a way to repeat the entry multiple times without restarting the application.
-//* CANNOT use any Extensions
-//* Must use the Array DataType
-//    * CANNOT use any of the Collection Object Types.
-//* Any Methods/Functions must be directly accessible from the Object/Type being used
-//    > Can be Static or Instance based.
-
-//# Deliverables (Attached to Assignment)
-//* Word Document
-//    * Contains link to your GitHub Repository for the Homework Assignment
-//    * Document any problems you struggled with.
-
 namespace HW1Array
 {
     internal class Program
